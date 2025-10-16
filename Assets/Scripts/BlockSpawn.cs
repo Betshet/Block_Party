3b084f2b28ed6f3909b07e58bbae332087a154fe
a2ri1 <adri-xd@hotmail.fr>
2025-10-16 23:00:09 +0200

    Ajout de qq sons

--- Assets/Scripts/BlockSpawn.cs
+++ Assets/Scripts/BlockSpawn.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
